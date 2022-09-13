15613e5a917862fe3636231ad6b8e064e436528c
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2022-09-13 16:37:59 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,10 +6,13 @@
  */
 int main(void)
 {
-	_putchar('A');
-	_putchar('L');
-	_putchar('X');
-	_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('r');
 
 	return (0);
 }
