e105639a6a1ce0bc45aeecbd517bb69818a524c4
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2022-10-25 01:17:44 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,6 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
