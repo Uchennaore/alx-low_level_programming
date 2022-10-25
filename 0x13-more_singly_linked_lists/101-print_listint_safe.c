4dae25799b29f88a4ed9b7656f2f1e1fe88acf56
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2022-10-25 01:19:54 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,78 +1,29 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_nodeaddress - Add a node to a list with
- * @head: Pointer to the pointer of the start of the list
- * @address: The address of another list's node
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
  *
- * Return: Address of new node, NULL if it fails
- */
-listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address)
-{
-	listadd_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		free_listadd(*head);
-		exit(98);
-	}
-	new->address = (void *)address;
-	new->next = *head;
-	*head = new;
-	return (new);
-}
-
-/**
- * free_listadd - Free a list
- * @head: Pointer to the start of the list
- */
-void free_listadd(listadd_t *head)
-{
-	listadd_t *killnode;
-
-	while (head != NULL)
-	{
-		killnode = head;
-		head = head->next;
-		free(killnode);
-	}
-}
-
-/**
- * print_listint_safe - Print out a given list, but only once if it loops
- * @head: Pointer to the start of the list
- *
- * Return: Number of nodes, if it fails print 98
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	listadd_t *newhead;
-	listadd_t *checker;
-	unsigned int count;
+	size_t num = 0;
+	long int diff;
 
-	count = 0;
-	newhead = NULL;
-	while (head != NULL)
+	while (head)
 	{
-		checker = newhead;
-		while (checker != NULL)
-		{
-			if (head == checker->address)
-			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listadd(newhead);
-				return (count);
-			}
-			checker = checker->next;
-		}
+		diff = head - head->next;
+		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		add_nodeaddress(&newhead, head);
-		head = head->next;
-		count++;
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
 	}
-	free_listadd(newhead);
-	return (count);
+
+	return (num);
 }
